1775498e0d7f453372e4ef5a6189da9256299d63
HenryCodeT <120686@unsaac.edu.pe>
2022-01-09 13:53:19 -0500

    parte 2

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-let elementLike = document.getElementById("likes");
-let count = 0;
-function addLike() {
-    count++
-    elementLike.innerText = `${count} like(s)`
+let element1 = document.getElementById("likes1");
+let element2 = document.getElementById("likes2");
+let element3 = document.getElementById("likes3");
+
+let count1 = 0;
+let count2 = 0;
+let count3 = 0;
+
+function addLike(event) {
+    if (event.id==="btn-1") {
+        count1++
+        element1.innerText=`${count1} like(s)`
+    }else if(event.id==="btn-2"){
+        count2++
+        element2.innerText=`${count2} like(s)`
+    }else if(event.id==="btn-3"){
+        count3++
+        element3.innerText=`${count3} like(s)`
+    }
 }
 function addStyleOver(event) {
     event.classList.replace("btn-1","btn-2")
