16c14e899e13b37b534502b717f93a27d76fa843
HenryCodeT <120686@unsaac.edu.pe>
2022-01-11 18:03:55 -0500

    refactores script.js with method closest

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-let element1 = document.getElementById("likes1");
-let element2 = document.getElementById("likes2");
-let element3 = document.getElementById("likes3");
-
-let count1 = 0;
-let count2 = 0;
-let count3 = 0;
-
-function addLike(event) {
-    if (event.id==="btn-1") {
-        count1++
-        element1.innerText=`${count1} like(s)`
-    }else if(event.id==="btn-2"){
-        count2++
-        element2.innerText=`${count2} like(s)`
-    }else if(event.id==="btn-3"){
-        count3++
-        element3.innerText=`${count3} like(s)`
-    }
+let count = 0;
+function addLike(element) {
+    let prevSibling = element.closest(".card-user").querySelector("#like")
+    count++
+    prevSibling.innerText=`${count} like(s)`
 }
 function addStyleOver(event) {
     event.classList.replace("btn-1","btn-2")
